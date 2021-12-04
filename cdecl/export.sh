b86a371fd7a95fdde091f002ec78b42870791edd
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2021-12-05 02:23:14 +0800

    Use docker cache to remember latest commits, gj!

---
+++ cdecl/export.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p /out/bin /out/man/man1
+cp /repo/src/cdecl /out/bin/
+cp /repo/man/man1/c*decl.1 /out/man/man1
---
+++ dtach/export.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p /out/bin /out/man/man1
+cp /repo/dtach /out/bin/
+cp /repo/dtach.1 /out/man/man1
